1e4b3a26a3ca2e22597e8ffea7fdaef6b3991788
Michael Boesherz <michael.boesherz@numero2.de>
2022-07-19 11:15:14 +0200

    first changes for contao 5

---
+++ src/DependencyInjection/ProperFilenamesExtension.php
@@ -0,0 +1,39 @@
+<?php
+
+/**
+ * Contao Open Source CMS
+ *
+ * Copyright (c) 2005-2022 Leo Feyer
+ *
+ * @package   Contao Marketing Suite
+ * @author    Benny Born <benny.born@numero2.de>
+ * @author    Michael Bösherz <michael.boesherz@numero2.de>
+ * @license   Commercial
+ * @copyright 2022 numero2 - Agentur für digitales Marketing
+ */
+
+
+namespace numero2\ProperFilenamesBundle\DependencyInjection;
+
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\Extension;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+
+
+class ProperFilenamesExtension extends Extension {
+
+
+    /**
+     * {@inheritdoc}
+     */
+    public function load(array $mergedConfig, ContainerBuilder $container): void {
+
+        $loader = new YamlFileLoader(
+            $container,
+            new FileLocator(__DIR__.'/../Resources/config')
+        );
+
+        // $loader->load('listener.yml');
+    }
+}
